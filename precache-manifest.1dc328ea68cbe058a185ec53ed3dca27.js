a4e7227ff3639e004800f2d3e87e60c62a19b28d
jonniek01 <jontejonhn@gmail.com>
2022-06-28 15:08:32 +0300

    Updates

---
+++ precache-manifest.1dc328ea68cbe058a185ec53ed3dca27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a827fc759e1de3cc387",
+    "url": "/movie-app-redux/static/css/main.fe623d00.chunk.css"
+  },
+  {
+    "revision": "5a827fc759e1de3cc387",
+    "url": "/movie-app-redux/static/js/main.5a827fc7.chunk.js"
+  },
+  {
+    "revision": "ad39ee43e5f1e39ec27e",
+    "url": "/movie-app-redux/static/js/1.ad39ee43.chunk.js"
+  },
+  {
+    "revision": "080f45e56c4a826dcebf",
+    "url": "/movie-app-redux/static/js/2.080f45e5.chunk.js"
+  },
+  {
+    "revision": "07fd2bc00c1be83bc46d",
+    "url": "/movie-app-redux/static/js/runtime~main.07fd2bc0.js"
+  },
+  {
+    "revision": "acdc75e186dbea62e10f9da55bef3fac",
+    "url": "/movie-app-redux/index.html"
+  }
+];
